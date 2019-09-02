b70a1752890e4d04e65a4322696c5e001f55769e
sk-sahu <sk-sahu@github.com>
2019-09-03 00:05:49 +0530

    Updates

---
+++ precache-manifest.79e51e053830c331bfddb56628d087ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e1fb498d2d87985e1854007392b5602",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "2e63cc0d341d19fad1ba",
+    "url": "/my-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "4a730d5c2f1f8fd2b755",
+    "url": "/my-app/static/js/2.65e324cd.chunk.js"
+  },
+  {
+    "revision": "2e63cc0d341d19fad1ba",
+    "url": "/my-app/static/js/main.1c81ebbf.chunk.js"
+  },
+  {
+    "revision": "00fa2de64e66be72c556",
+    "url": "/my-app/static/js/runtime~main.122bc8ec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
